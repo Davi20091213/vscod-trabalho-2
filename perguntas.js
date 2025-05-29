340f1faac8db5323aabb8ef821878f671caf289f
Davi20091213 <davi.nunes.oliveira@escola.pr.gov.br>
2025-05-29 12:38:15 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Matemática 1',
+    'O que é um problema convencional?',
+    'Ele possui somente uma solução e informaçoẽs suficientes para resolver-lo'
 )
 
 criaCartao(
@@ -20,4 +20,39 @@ criaCartao(
     'Lingua inglesa',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Sociologia',
+    'Objeto de estudo de Émile Durkein?',
+    'Fatos sociais !!'
+)
+criaCartao(
+    'Potuguês',
+    'O que seria um argumento subjetivo ?',
+    'Argumento baseado em algo que a pessoa acredita'
+)
+criaCartao(
+    'Física 1',
+    'Qual nome do Software de programação?',
+    'Arduino IDE'
+)
+criaCartao(
+    'Química',
+    'Qual nome da ligação iônica que compartilha elétrons?',
+    'LIgação Covalente'
+)
+criaCartao(
+    'Educação Financeira',
+    'quanto é 1.000 dólares em reais?',
+    '5.061 reais'
+)
+criaCartao(
+    'Biologia',
+    'Nome do Grupo das araucárias?',
+    'Gmniospermas'
+)
+criaCartao(
+    'Matemática 2',
+    'O que é Java script?',
+    'Linguagem da programação'
 )
