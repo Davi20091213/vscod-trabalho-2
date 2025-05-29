482886f7c3d2a0edad0007fb1b4b54c67f540de2
Davi20091213 <davi.nunes.oliveira@escola.pr.gov.br>
2025-05-29 11:02:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 79²?',
+    '6241 !!!'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
